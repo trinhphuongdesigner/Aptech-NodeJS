4404b65583ba81d75ed123cad10e59fe37791533
@phuongtd.wiicamp <phuongtd@wiicamp.com>
2023-04-10 18:05:02 +0700

    Update socket

--- Batch27/backend-express/app.js
+++ Batch27/backend-express/app.js
@@ -5,6 +5,7 @@ const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 const cors = require('cors');
 const passport = require('passport');
+
 require('dotenv').config()
 
 // MONGOOSE
@@ -27,6 +28,22 @@ const questionsRouter = require('./routes/questions');
 const uploadRouter = require('./routes/upload');
 
 const app = express();
+
+// const server = http.Server(app);
+// const io = socketIO(server);
+
+// io.on('connection', (socket) => {
+//   console.log('A client connected.');
+
+//   socket.on('chat', (msg) => {
+//     console.log(`Received message: ${msg}`);
+//     io.emit('chat message', msg);
+//   });
+
+//   socket.on('disconnect', () => {
+//     console.log('A client disconnected.');
+//   });
+// });
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -75,6 +92,10 @@ app.use('/orders', ordersRouter);
 app.use('/questions', questionsRouter);
 
 app.use('/upload', uploadRouter);
+
+app.get('/chat', (req, res) => {
+  res.sendFile(__dirname + '/index.html')
+})
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
